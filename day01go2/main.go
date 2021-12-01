3434cd800093dbfa047113540eee57324a022881
Andzhey Asryan <andzhey.asryan@freiheit.com>
2021-12-01 09:54:43 +0100

    Day 02b

---
+++ day01go2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"advents2021/util"
+	"fmt"
+)
+
+func main() {
+	input := util.ReadFile("./day01go1/input.txt")
+	fmt.Println(increased(input))
+}
+
+func increased(ss []string) int {
+	xx := append(ss, "0", "0")
+	var count int
+	for i := range ss {
+		if i == 0 {
+			continue
+		}
+		if sum(xx, i) > sum(xx, i-1) {
+			count++
+		}
+	}
+	return count
+}
+
+func sum(ss []string, i int) int {
+	return util.ToInt(ss[i]) + util.ToInt(ss[i+1]) + util.ToInt(ss[i+2])
+}
