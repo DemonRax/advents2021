55da6d5290f925db6d99a3ebb2ce4bd6be5a7a39
Andzhey Asryan <andzhey.asryan@freiheit.com>
2021-12-01 09:48:06 +0100

    Init Day 01a

---
+++ day01go1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"advents2021/util"
+	"fmt"
+)
+
+func main() {
+	input := util.ReadFile("./day01go1/input.txt")
+	fmt.Println(increased(input))
+}
+
+func increased(ss []string) int {
+	var count int
+	for i := range ss {
+		if i == 0 {
+			continue
+		}
+		if util.ToInt(ss[i]) > util.ToInt(ss[i-1]) {
+			count++
+		}
+	}
+	return count
+}
---
+++ day01go1/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+
+	"advents2021/util"
+)
+
+func Test_code(t *testing.T) {
+	for _, test := range []struct {
+		in   string
+		want int
+	}{
+		{
+			in: `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`,
+			want: 7,
+		},
+	} {
+		t.Run(test.in, func(t *testing.T) {
+			if got := increased(util.ReadString(test.in)); got != test.want {
+				t.Errorf("got = %d, want %d", got, test.want)
+			}
+		})
+	}
+}
