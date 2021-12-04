4f4a6a862cc7a0280c3608318af3df9e44b503e0
Andzhey Asryan <andzhey.asryan@freiheit.com>
2021-12-04 22:26:45 +0100

    Day 04a

---
+++ day04go1/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"advents2021/util"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := util.ReadFile("./day04go1/input.txt")
+	fmt.Println(play(parseBingo(input)))
+}
+
+func play(nums []int, cards []card) int {
+	current := make([]int, 0, len(nums))
+	for _, n := range nums {
+		current = append(current, n)
+		for _, card := range cards {
+			sum, win := card.scan(current)
+			if sum != 0 && win != 0 {
+				return sum * win
+			}
+		}
+	}
+	return 0
+}
+
+type card [][]int
+
+func (c card) scan(nums []int) (int, int) {
+	sum, win := 0, -1
+
+	colHits := make([]int, 5)
+	for _, row := range c {
+		rowHits := 0
+		for i, col := range row {
+			hit := false
+			for _, num := range nums {
+				hit = hit || col == num
+			}
+			if hit {
+				rowHits++
+				colHits[i]++
+			} else {
+				sum += col
+			}
+		}
+		if rowHits == 5 {
+			win = nums[len(nums)-1]
+		}
+	}
+	for _, wins := range colHits {
+		if wins == 5 {
+			win = nums[len(nums)-1]
+		}
+	}
+
+	if win < 0 {
+		win, sum = 0, 0
+	}
+	return sum, win
+}
+
+func parseBingo(ss []string) ([]int, []card) {
+	nums := strings.Split(ss[0], ",")
+	numbers := make([]int, len(nums))
+	for i, num := range nums {
+		numbers[i], _ = strconv.Atoi(num)
+	}
+
+	cards := make([]card, 0, len(ss))
+	var row int
+	c := make([][]int, 5)
+	for _, line := range ss[1:] {
+		if line == "" {
+			continue
+		}
+
+		c[row] = make([]int, 5)
+		var col int
+		nums := strings.Split(line, " ")
+		for _, num := range nums {
+			var err error
+			c[row][col], err = strconv.Atoi(num)
+			if err == nil {
+				col++
+			}
+		}
+		row++
+		if row == 5 {
+			cards = append(cards, c)
+			c = make([][]int, 5)
+			row = 0
+		}
+	}
+	return numbers, cards
+}
---
+++ day04go1/main_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"advents2021/util"
+	"testing"
+)
+
+func Test_code(t *testing.T) {
+	for _, test := range []struct {
+		in   string
+		want int
+	}{
+		{
+			in: `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+`,
+			want: 4512,
+		},
+		{
+			in: `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+10 18 14 22  2
+16  8 21 11  0
+15 23 17 13 12
+ 9 26 24  6  3
+19 20  4  5  7`,
+			want: 4512,
+		},
+	} {
+		t.Run(test.in, func(t *testing.T) {
+			if got := play(parseBingo(util.ReadString(test.in))); got != test.want {
+				t.Errorf("got = %d, want %d", got, test.want)
+			}
+		})
+	}
+}
+
+func Test_scan(t *testing.T) {
+	for _, test := range []struct {
+		desc    string
+		card    card
+		nums    []int
+		wantSum int
+		wantWin int
+	}{
+		{
+			desc: "sample 1 no win",
+			card: card{
+				{10, 16, 15, 9, 19},
+				{14, 21, 17, 24, 4},
+				{18, 8, 23, 26, 20},
+				{22, 11, 13, 6, 5},
+				{2, 0, 12, 3, 7},
+			},
+			nums: []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21},
+		},
+		{
+			desc: "sample 2 row win",
+			card: card{
+				{14, 21, 17, 24, 4},
+				{10, 16, 15, 9, 19},
+				{18, 8, 23, 26, 20},
+				{22, 11, 13, 6, 5},
+				{2, 0, 12, 3, 7},
+			},
+			nums:    []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24},
+			wantSum: 188,
+			wantWin: 24,
+		},
+		{
+			desc: "sample 2 col win",
+			card: card{
+				{14, 10, 18, 22, 2},
+				{21, 16, 8, 11, 0},
+				{17, 15, 23, 13, 12},
+				{24, 9, 26, 6, 3},
+				{4, 19, 20, 5, 7},
+			},
+			nums:    []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24},
+			wantSum: 188,
+			wantWin: 24,
+		},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			gotSum, gotWin := test.card.scan(test.nums)
+			if gotSum != test.wantSum {
+				t.Errorf("sum = %d want %d", gotSum, test.wantSum)
+			}
+			if gotWin != test.wantWin {
+				t.Errorf("win = %d want %d", gotWin, test.wantWin)
+			}
+		})
+	}
+}
